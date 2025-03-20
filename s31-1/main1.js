b1052d0d513582bc1a0aac972acaefa0afabc738
đặng mạnh trường <manhtruong2497@gmail.com>
2025-03-20 08:10:22 +0700

    s31

--- s31-1/main1.js
+++ s31-1/main1.js
@@ -1,1 +1,7 @@
-console.log
+document.getElementById("btn-hide").onclick = function () {
+    document.getElementById("text").style.display = "none";
+}
+
+document.getElementById("btn-show").onclick = function () {
+    document.getElementById("text").style.display = "flex";
+}
